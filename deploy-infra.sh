3bd442db758d54324741431d414879d9ff2844ea
Brian Jenkins <bonkydog@bonkydog.com>
2020-02-01 05:55:02 +0000

    Add codebuild / codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -13,11 +13,28 @@ fi
 
 STACK_NAME=$1
 CLI_PROFILE=bonkydog
+REGION=us-west-2
 EC2_INSTANCE_TYPE=t2.micro
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
+  --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
   --template-file main.yml \
@@ -26,11 +43,12 @@ aws cloudformation deploy \
   --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE
 
+
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
-    --profile "$STACK_NAME" \
-    --query "Exports[?Name=='InstanceEndpoint'].Value"
+    --profile $CLI_PROFILE \
+    --query "Exports[?Name=='${STACK_NAME}-endpoint'].Value"
 else
   aws cloudformation describe-stack-events --stack-name "$STACK_NAME"
 fi
